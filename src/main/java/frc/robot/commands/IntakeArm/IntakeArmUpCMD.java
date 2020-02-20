2751de8614a32734bb36da59488da1927ed6fe24
Massinissa Bosli <MaBosli@sau57.net>
2020-02-19 19:24:42 -0500

    isha is a dumbo and needs to die

--- src/main/java/frc/robot/commands/IntakeArm/IntakeArmUpCMD.java
+++ src/main/java/frc/robot/commands/IntakeArm/IntakeArmUpCMD.java
@@ -26,7 +26,7 @@ public class IntakeArmUpCMD extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    Robot.intakearm.armIntakeUp();
+   // Robot.intakearm.armIntakeUp();
   }
 
   // Called once the command ends or is interrupted.
